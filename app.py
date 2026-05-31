e26f1708347d5f167ca12c65e0eb803a8f59849d
anushka_chavare <anushkachavare25@gmail.com>
2026-05-31 15:52:50 +0530

    Implement extractive and transformer summarization

--- app.py
+++ app.py
@@ -1,35 +1,74 @@
+##EXTRACTIVE SUMMARIZATION (NLTK)
+
+import streamlit as st
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize, sent_tokenize
+import heapq
+
+st.title("Text Summarizer (Extractive NLP)")
+
+text = st.text_area("Enter Text")
+
+if st.button("Summarize"):
+
+    if text.strip():
+
+        sentences = sent_tokenize(text)
+
+        stop_words = set(stopwords.words("english"))
+
+        word_freq = {}
+
+        for word in word_tokenize(text.lower()):
+            if word.isalnum() and word not in stop_words:
+                word_freq[word] = word_freq.get(word, 0) + 1
+
+        sentence_scores = {}
+
+        for sentence in sentences:
+            for word in word_tokenize(sentence.lower()):
+                if word in word_freq:
+                    sentence_scores[sentence] = (
+                        sentence_scores.get(sentence, 0)
+                        + word_freq[word]
+                    )
+
+        summary = heapq.nlargest(
+            3,
+            sentence_scores,
+            key=sentence_scores.get
+        )
+
+        st.subheader("Summary")
+        st.write(" ".join(summary))
+
+##TRANSFORMERS SUMMARIZATION (BART)
+import streamlit as st
 from transformers import pipeline
-from PyPDF2 import PdfReader
 
-# Load summarization model
-summarizer = pipeline(
-    "summarization",
-    model="facebook/bart-large-cnn"
-)
+st.title("AI Text Summarizer (Transformer)")
 
-# Read PDF file
-reader = PdfReader(r"C:\Users\anush\OneDrive\Desktop\CPE\Weekly Diary finl.pdf")
+text = st.text_area("Enter Text")
 
-text = ""
+@st.cache_resource
+def load_model():
+    return pipeline(
+        "summarization",
+        model="facebook/bart-large-cnn"
+    )
 
-# Extract text from all pages
-for page in reader.pages:
-    extracted_text = page.extract_text()
+summarizer = load_model()
 
-    if extracted_text:
-        text += extracted_text
+if st.button("Summarize"):
 
-# Limit text size (important for transformer models)
-text = text[:2000]
+    if text.strip():
 
-# Generate summary
-summary = summarizer(
-    text,
-    max_length=120,
-    min_length=40,
-    do_sample=False
-)
+        summary = summarizer(
+            text,
+            max_length=100,
+            min_length=30,
+            do_sample=False
+        )
 
-# Print summary
-print("\n========== SUMMARY ==========\n")
-print(summary[0]['summary_text'])
+        st.subheader("Summary")
+        st.write(summary[0]["summary_text"])
